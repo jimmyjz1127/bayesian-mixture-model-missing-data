b586a58f795c0f5dbcf1b476e4c4497ad2f92ea1
Jimmy Zhang <jimmyjz1127@gmail.com>
2025-07-28 19:08:26 +0100

    Implemented synthetic data generation

--- datasets/Dataset.py
+++ datasets/Dataset.py
@@ -2,25 +2,22 @@ import numpy as np
 import pandas as pd
 
 class Dataset:
-    def __init__(self, X,y, missing_rate=0.1, seed=5099):
+    def __init__(self, X,y, seed=5099):
         self.X_full       = X
         self.y            = y
         self.seed         = seed
 
-        rng = np.random.default_rng(self.seed)
+        self.rng = np.random.default_rng(self.seed)
 
-    def apply_missingness(self, X, missing_rate, missing_type="uniform"):
-        mask = np.ones_like(X, dtype=bool)
+    def apply_missingness(self, missing_rate, missing_type="uniform"):
+        mask = np.ones_like(self.X, dtype=bool)
 
         if missing_type == "uniform":
-            mask = self.rng.random(X.shape) > missing_rate
+            mask = self.rng.random(self.X.shape) > missing_rate
 
-        X_missing = X.copy()
+        X_missing = self.X.copy()
         X_missing[~mask] = np.nan
         return X_missing,self.y
-    
-    def get_missing_data(self):
-        return self.X_missing
 
     def get_complete_data(self):
         return self.X_full
